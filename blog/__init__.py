e61cd85fea65b8da8e18cee966333af304f497bb
seangongz <sean.gongz@gmail.com>
2016-03-25 22:57:28 +0100

    create the database

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# ------------------------------------------- #
+# tutorial                                    #
+# from flask.ext.sqlalchemy import SQLAlchemy #
+# ------------------------------------------- #
+
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from blog import views
+from blog import views, models
